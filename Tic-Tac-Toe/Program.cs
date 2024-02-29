5a4d5a964a9bf17a1a1335aecd2e90bbecf836c4
Pascal de Wit <Pascaldewit@outlook.com>
2024-03-01 00:32:04 +0100

    Finished #2 & #3

--- Tic-Tac-Toe/Program.cs
+++ Tic-Tac-Toe/Program.cs
@@ -1,2 +1,54 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace TicTacToe
+{
+    class Program
+    {
+        static void Main()
+        {
+            //UI.Welcome();
+            Board.DrawBoard();
+        }
+    }
+
+    class UI
+    {
+        public static void Welcome()
+        {
+            Console.WriteLine("Welcome to Ultimate Tic-Tac-Toe");
+            Console.WriteLine("Press any key to start...");
+
+            Console.ReadKey();
+        }
+    }
+
+    class Board
+    {
+        static char[,] board = new char[3, 3]
+        {
+            { ' ', ' ', ' '},
+            { ' ', ' ', ' '},
+            { ' ', ' ', ' '}
+        };
+
+        public static void DrawBoard()
+        {
+            Console.Clear();
+            Console.WriteLine("  0 1 2");
+
+            for (int row = 0; row < board.GetLength(0); row++)
+            {
+                Console.Write(row + "|");
+
+                for (int col = 0; col < board.GetLength(1); col++)
+                {
+                    Console.Write(board[row, col]);
+                    // Prevents an extra column from being drawn, hence the -1.
+                    if (col < board.GetLength(1) - 1)
+                    {
+                        Console.Write("|");
+                    }
+                }
+                Console.WriteLine("|");
+            }
+        }
+    }
+}
